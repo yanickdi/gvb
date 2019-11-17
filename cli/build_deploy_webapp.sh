153f1c34c407ec5a2779aed81b8e553b891bc97f
yanick <dickbauer.yanick@gmail.com>
2019-11-17 19:16:51 +0100

    improved backend

---
+++ cli/build_deploy_webapp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+source env/secrets.sh
+
+build() {
+  (
+    echo "BUILDING FRONTEND"
+    cd frontend || exit
+    #export PUBLIC_URL="https://yanick.dickbauer.at/fahrplan/"
+    yarn build
+  )
+}
+
+deploy(){
+  echo "DEPLOYING FRONTEND"
+  rsync -a frontend/build/ $REMOTE_SERVER:$REMOTE_ROOT/www-root
+
+  echo "DEPLOYING BACKEND"
+  # TODO: do not copy vendor files, instead copy them on the server side
+  rsync -a backend/ $REMOTE_SERVER:$REMOTE_ROOT/api
+}
+
+#build
+deploy
