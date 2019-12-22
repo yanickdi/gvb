2fdb8d2a54c8f95c273e6eb459b0e90718a7a779
yanick <dickbauer.yanick@gmail.com>
2019-12-22 13:53:02 +0100

    Backend: Added heroku psql support

--- backend/app/application.py
+++ backend/app/application.py
@@ -8,8 +8,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 basedir = os.path.dirname(os.path.abspath(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(
-    os.path.join(basedir, "..", "db.sqlite"))
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    os.environ.get('DATABASE_URL', 'sqlite:///{}'.format(
+        os.path.join(basedir, "..", "db.sqlite")))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -17,7 +18,6 @@ ma = Marshmallow(app)
 
 # apply cors middleware
 CORS(app)
-
 
 # import views
 from app import views
